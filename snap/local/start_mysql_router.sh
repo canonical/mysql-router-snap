35956059541e60570a4c7cc25cd70aa21932cd67
Jarred Wilson <jarred.wilson12@gmail.com>
2023-01-18 10:54:33 -0500

    Remove checking and change ownership automatically

--- snap/local/start_mysql_router.sh
+++ snap/local/start_mysql_router.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-if ! [[ -O $SNAP_COMMON/mysqlrouter ]]; then
-        # Change ownership of snap directories to allow snap_daemon to read/write
-        chown -R snap_daemon:root $SNAP_DATA
-        chown -R snap_daemon:root $SNAP_COMMON
-fi
+# Change ownership of snap directories to allow snap_daemon to read/write
+chown -R snap_daemon:root $SNAP_DATA
+chown -R snap_daemon:root $SNAP_COMMON
 
 # For security measures, daemons should not be run as sudo. Execute mongod as the non-sudo user: snap-daemon.
 $SNAP/usr/bin/setpriv --clear-groups --reuid snap_daemon \
